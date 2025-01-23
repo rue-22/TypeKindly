a087bc679b7d085a8bb12e7633167b8480720725
rue-22 <cssalces@up.edu.ph>
2025-01-23 11:20:05 +0800

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+	import MenuChoice from '../routes/MenuChoice.svelte';
+</script>
+
+<main class="min-height[calc(100vh-0.5rem)] flex min-h-screen flex-col">
+	<header class="sticky top-0 w-full bg-black py-[0.5rem] text-center text-white">
+		<h1>TypeKindly</h1>
+	</header>
+
+	{@render children()}
+
+	<navbar
+		class="sticky bottom-0 flex w-full flex-row justify-around gap-4 bg-black py-[0.5rem] text-white"
+	>
+		<MenuChoice menuName="home" />
+		<MenuChoice menuName="records" />
+		<MenuChoice menuName="dictionary" />
+		<MenuChoice menuName="settings" />
+	</navbar>
+
+	<!-- <footer class="min-h-5em sticky bottom-0 bg-black text-white">
+		<p>This is the sticky footer.</p>
+	</footer> -->
+</main>
---
+++ src/routes/MenuChoice.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	let { menuName } = $props();
+
+	import book from '/static/book.svg?raw';
+	import calendar from '/static/calendar.svg?raw';
+	import home from '/static/home.svg?raw';
+	import settings from '/static/settings.svg?raw';
+</script>
+
+<div>
+	<a
+		href={menuName === 'home' ? '/' : `/${menuName}`}
+		class="align-center gap flex h-full flex-col items-center rounded-xl border-2 p-4"
+	>
+		{#if menuName == 'home'}
+			{@html home}
+		{:else if menuName == 'records'}
+			{@html calendar}
+		{:else if menuName == 'dictionary'}
+			{@html book}
+		{:else if menuName == 'settings'}
+			{@html settings}
+		{/if}
+
+		<p>{menuName.charAt(0).toUpperCase() + menuName.slice(1)}</p>
+	</a>
+</div>
