f5f21cb8fdae14f8b2723af0808fb2af6308175c
ligsqt <115415483+LigsQt@users.noreply.github.com>
2025-01-23 17:15:53 +0800

    feat: initialize android setup

---
+++ android/app/src/main/java/com/typekindly/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.typekindly.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.typekindly.app',
+  appName: 'typekindly',
+  webDir: 'build'
+};
+
+export default config;
