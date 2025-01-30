206eedb0f848775409aaa62af4781573d191cee5
rue-22 <cssalces@up.edu.ph>
2025-01-30 20:09:12 +0800

    feat: add textarea for easier debugging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Hello, world!</h1>
+<script lang="ts">
+	let textInput = $state('');
+</script>
 
+<div class="mt-12 flex flex-col items-center justify-center gap-4 text-white">
+	<textarea class="min-h-32 min-w-full bg-tkd-surface" bind:value={textInput}></textarea>
+
+	<button
+		class="rounded-md border-2 px-3 py-4"
+		onclick={() => {
+			textInput = '';
+		}}>Reset uwu</button
+	>
+</div>
+
+<p class="mt-8 text-center text-white">{textInput}</p>
