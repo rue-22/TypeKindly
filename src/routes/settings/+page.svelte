b0636674d4bd25503357a993838d7751d6f8f79e
rue-22 <cssalces@up.edu.ph>
2025-02-04 20:57:12 +0800

    feat: add description to explicit mode

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -3,10 +3,21 @@
 </script>
 
 <!-- EXPLICIT MODE -->
-<label class="inline-flex cursor-pointer items-center justify-between px-4 py-4">
-	<span class="text-xl text-white">Explicit Mode</span>
-	<input type="checkbox" bind:checked={$isExplicitOn} value="" class="peer sr-only" />
-	<div
-		class="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-tkd-primary peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-checked:bg-tkd-primary dark:peer-focus:ring-blue-800"
-	></div>
-</label>
+<div class="mt-3 flex flex-col gap-4">
+	<div class="rounded-md bg-tkd-surface px-4 py-4 text-justify text-white">
+		<label class="flex cursor-pointer items-center justify-between">
+			<span class="text-xl font-semibold">Explicit Mode</span>
+			<input type="checkbox" bind:checked={$isExplicitOn} value="" class="peer sr-only" />
+			<div
+				class="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-tkd-primary peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-checked:bg-tkd-primary dark:peer-focus:ring-blue-800"
+			></div>
+		</label>
+
+		<p class="mt-2">
+			When <span class="font-semibold">Explicit Mode</span> is turned on, bad words in the dictionary
+			will be visible. Turn this setting off to hide them, especially when working with children or minors.
+		</p>
+	</div>
+
+	<div class="rounded-md bg-tkd-surface px-4 py-4 text-justify text-white">Another Setting</div>
+</div>
