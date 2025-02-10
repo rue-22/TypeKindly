6efeb53a87a2f70a030325ff6aa2a1da8de088b7
rue-22 <cssalces@up.edu.ph>
2025-02-10 21:05:08 +0800

    feat: add initial export database setting

---
+++ src/lib/components/ExportDatabase.svelte
@@ -0,0 +1,7 @@
+<div class="rounded-md bg-tkd-surface px-4 py-4 text-justify text-white">
+	<h1 class="text-xl font-semibold">Export Your Data</h1>
+	<p class="mt-2">
+		Your data, your right! If you wish to obtain your typing data that this application gathered,
+		click this button to download your data. (?????)
+	</p>
+</div>
