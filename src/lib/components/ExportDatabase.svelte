010809c3b494241f14ca2210cf2ae9a60ff3e4f3
rue-22 <cssalces@up.edu.ph>
2025-02-12 23:04:39 +0800

    fix: make export UI more okay

--- src/lib/components/ExportDatabase.svelte
+++ src/lib/components/ExportDatabase.svelte
@@ -3,9 +3,14 @@
 </script>
 
 <div class="rounded-md bg-tkd-surface px-4 py-4 text-justify text-white">
-	<button onclick={exportData} class="border-2 text-xl font-semibold">Export Your Data</button>
+	<button
+		type="button"
+		onclick={exportData}
+		class="mb-2 me-2 w-full rounded-2xl bg-gray-800 px-5 py-2.5 text-xl font-semibold text-white hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-tkd-surface dark:border-tkd-surface dark:bg-tkd-background dark:hover:bg-tkd-background dark:focus:ring-tkd-background"
+		>Export your Data</button
+	>
 	<p class="mt-2">
-		Your data, your right! If you wish to obtain your typing data that this application has gathered since installation,
-		click the button above to start download.
+		Your data, your right! If you wish to obtain your typing data that this application has gathered
+		since installation, click the button above to start download.
 	</p>
 </div>
